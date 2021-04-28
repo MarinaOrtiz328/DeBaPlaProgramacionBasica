d59b811d06204938aaf8ea02f8b3e87ed7297fdb
Marina Ortiz <a329577@uach.mx>
2021-04-27 22:04:20 -0600

    38.-Ordenar  una  matriz  de  M  filas  y  N  columnas  por  la  primera  columna  utilizando  el  método SHELL (por inserción).

---
+++ 38.js
@@ -0,0 +1,22 @@
+const shellSort = arr => {
+  const gaps = [5, 3, 1];
+  for ( let g = 0; g < gaps.length; ++g ) {
+    for ( let i = gaps[ g ]; i < arr.length; ++i ) {
+      const temp = arr[ i ];
+      let j = i;
+      for ( ; j >= gaps[ g ] &&
+           arr[j-gaps[ g ] ] > temp;
+           j -= gaps[ g ] ) {
+        arr[ j ] = arr[ j - gaps[ g ] ];
+      }
+      arr[ j ] = temp;
+    }
+  }
+
+  return arr;
+}
+
+
+const arr = [10, 4, 40, 32, 67, 12, 43, 31, 65, 1];
+const resultado = shellSort(arr);
+console.log(resultado);
