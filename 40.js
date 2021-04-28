e5d03258fa87c37d0f6834b32f38675f4e345309
Marina Ortiz <a329577@uach.mx>
2021-04-27 22:30:28 -0600

    40.-  Una  empresa  guarda  las  ventas  realizadas  por  sus  tres  representantes  a  lo  largo  de  doce meses de sus cuatro productos, VENTAS( representante, mes, producto ). Queremos proyectar el total de ventas, TOTAL ( mes, producto ), para lo cual sumamos las ventas de cada producto de cada mes de todos los representantes. Imprimir toda la información.

---
+++ 40.js
@@ -0,0 +1,31 @@
+function ventasTot(num, sellers) {
+    let productos = [];
+    for (let i = 0; i < num; i++) {
+        productos.push([]);
+        for (let j = 0; j < 12; j++) {
+            productos[i].push([]);
+            let ventas = Math.floor(Math.random() * 5) + 1;
+            for (let k = 0; k < ventas; k++) {
+                productos[i][j].push({
+                    vendedor: vendedores[Math.floor(Math.random() * sellers.length)],
+                    venta: Math.floor(Math.random() * 500)+1
+                });
+            }
+        }
+    }
+    return productos;
+}
+
+let months = ['Enero', 'Febrero','Marzo', 'Abril', 'Mayo','Junio', 'Julio', 'Agosto','Septiembre', 'Octubre','Noviembre', 'Diciembre'];
+let vendedores = ['Luis', 'Maria', 'Jesus']
+let ventas = ventasTot(4, vendedores);
+ventas.forEach((producto, numero) => {
+    console.log(`Ventas producto ${numero+1}`);
+    producto.forEach((mes, numero) => {
+        let total = mes.reduce((acc, current) => {
+            return acc + current.venta
+        }, 0);
+        console.log(`${months[numero]} : ${total}`);
+    });
+    console.log('\n');
+});
